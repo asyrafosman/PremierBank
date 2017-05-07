b67ac17b91a26b05bf973ae04edcd21499572077
MUHAMMAD SALMAN BIN MOHD SHAH <msalman26@live.utm.my>
2017-05-07 16:26:09 +0800

    GraphTest

---
+++ PremierBank/GraphTest.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GraphTest.aspx.cs" Inherits="PremierBank.GraphTest" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:scriptmanager runat="server"></asp:scriptmanager>
+        <asp:DropDownList ID="ddlCountry1" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddlCountry2" runat="server"></asp:DropDownList>
+        <asp:Button ID="btnCompare" runat="server" Text="Compare" OnClick="Compare" />
+        <cc1:LineChart ID="LineChart1" runat="server" ChartHeight="300" ChartWidth="450"
+                ChartType="Basic" ChartTitleColor="#0E426C" Visible="false" CategoryAxisLineColor="#D08AD9"
+                ValueAxisLineColor="#D08AD9" BaseLineColor="#A156AB"></cc1:LineChart>
+    </form>
+</body>
+</html>
---
+++ PremierBank/GraphTest.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PremierBank
+{
+    public partial class GraphTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string query = "select distinct TxAmount from Trans";
+                DataTable dt = GetData(query);
+                
+                ddlCountry1.DataSource = dt;
+                ddlCountry1.DataTextField = "TxAmount";
+                ddlCountry1.DataValueField = "TxAmount";
+                ddlCountry1.DataBind();
+
+                ddlCountry2.DataSource = dt;
+                ddlCountry2.DataTextField = "TxAmount";
+                ddlCountry2.DataValueField = "TxAmount";
+                ddlCountry2.DataBind();
+                ddlCountry2.Items[1].Selected = true;
+            }
+        }
+
+        protected void Compare(object sender, EventArgs e)
+        {
+            string query = string.Format("select TxAmount from Trans", ddlCountry1.SelectedItem.Value);
+            DataTable dt = GetData(query);
+
+            string[] x = new string[dt.Rows.Count];
+            decimal[] y = new decimal[dt.Rows.Count];
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    x[i] = dt.Rows[i][0].ToString();
+            //    y[i] = Convert.ToInt32(dt.Rows[i][1]);
+            //}
+            LineChart1.Series.Add(new AjaxControlToolkit.LineChartSeries { Name = ddlCountry1.SelectedItem.Value, Data = y });
+
+            query = string.Format("select TxAmount from Trans", ddlCountry2.SelectedItem.Value);
+            dt = GetData(query);
+
+            y = new decimal[dt.Rows.Count];
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    x[i] = dt.Rows[i][0].ToString();
+            //    y[i] = Convert.ToInt32(dt.Rows[i][1]);
+            //}
+            LineChart1.Series.Add(new AjaxControlToolkit.LineChartSeries { Name = ddlCountry2.SelectedItem.Value, Data = y });
+            LineChart1.CategoriesAxis = string.Join(",", x);
+
+            LineChart1.ChartTitle = string.Format("{0} and {1} Order Distribution", ddlCountry1.SelectedItem.Value, ddlCountry2.SelectedItem.Value);
+            LineChart1.Visible = true;
+        }
+
+        private static DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            string constr = ConfigurationManager.ConnectionStrings["PremierBankCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+    }
+}
